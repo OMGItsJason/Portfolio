4e890ebc92877f85cffbefa3a784b9d91bb9a175
Jason Maliquid <jasonm2822@gmail.com>
2024-01-02 20:48:43 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: ['public']
-		}
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
