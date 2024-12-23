2d3a7e7a1e29d0417e454e07ed19eeb8eeae07b3
Jason Maliquid <jasonm2822@gmail.com>
2024-12-23 13:10:06 +0800

    ✅ ShadcnUI

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@/*': './src/lib/*'
+		}
 	},
 	preprocess: vitePreprocess()
 };
